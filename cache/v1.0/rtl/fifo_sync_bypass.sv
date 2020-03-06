7cf48a4684e8eb2fa8cfdc62c10c7be8db45ff0e
ARF1939261764 <1939261764@qq.com>
2020-03-06 23:00:00 +0800

    带旁路的同步fifo完成

--- cache/v1.0/rtl/fifo_sync_bypass.sv
+++ cache/v1.0/rtl/fifo_sync_bypass.sv
@@ -1,3 +1,6 @@
+/**************************************************************************
+带旁路的同步fifo
+**************************************************************************/
 module fifo_sync_bypass #(
   parameter DEPTH=2,  /*允许为2,4,8,16*/
             WIDTH=32
@@ -12,10 +15,62 @@ module fifo_sync_bypass #(
   input  [WIDTH-1:0]  writeData,
   output [WIDTH-1:0]  readData
 );
+/***************************************************************************
+fif缓存区地址宽度
+***************************************************************************/
+localparam ADDR_WIDTH=$clog2(DEPTH)+1;
 
-reg[WIDTH-1:0] array[DEPTH-1:0];
+/***************************************************************************
+寄存器，线网
+***************************************************************************/
+reg   [WIDTH-1:0]        array[DEPTH-1:0];
+reg   [ADDR_WIDTH-1:0]   front,rear;
+wire  [ADDR_WIDTH-1:0]   count;
 
-assign readData=writeData;
+/***************************************************************************
+连线
+***************************************************************************/
+assign count  =  rear-front;
+assign full   =  (front[ADDR_WIDTH-1]^rear[ADDR_WIDTH-1])&&(front[ADDR_WIDTH-2:0]==rear[ADDR_WIDTH-2:0]);
+assign empty  =   front==rear;
+assign half   =  count>=DEPTH/2;
+
+/***************************************************************************
+选择器，选择读哪一个数据
+***************************************************************************/
+always @(*) begin
+  case(count)
+    0:begin
+      readData=writeData;
+    end
+    default:begin
+      readData=array[front[ADDR_WIDTH-2:0]];
+    end
+  endcase
+end
+
+/***************************************************************************
+fifo读写控制
+***************************************************************************/
+always @(posedge clk or negedge rest) begin:fifo_rw_block
+  if(!rest) begin:fifo_rw_rest_block
+    int i;
+    front<=0;
+    rear<=0;
+    for(i=0;i<DEPTH;i++) begin
+      array[i]<=0;
+    end
+  end
+  else begin
+    if(write&&!full&&((count!=0)||!read)) begin
+      array[rear[ADDR_WIDTH-2:0]]<=writeData;
+      rear++;
+    end
+    if(read&&!empty) begin
+      front++;
+    end
+  end
+end
 
 endmodule
 
