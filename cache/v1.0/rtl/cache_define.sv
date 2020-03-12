7066dfa131c38de22d13f2bbd36283e3043a1ef2
ARF1939261764 <1939261764@qq.com>
2020-03-12 14:24:44 +0800

    cache暂时到此为止，开始做CPU

--- cache/v1.0/rtl/cache_define.sv
+++ cache/v1.0/rtl/cache_define.sv
@@ -5,9 +5,6 @@
 
 `define CACHE_AVALON_BURST_COUNT_WIDTH          `AVALON_BURST_COUNT_WIDTH
 
-`define cache_io_cmd_wb                         (1)
-`define cache_io_cmd_clear                      (2)
-
 `define cache_ctr_cmd_nop                       (3'd0)
 `define cache_ctr_cmd_wb                        (3'd1)
 `define cache_ctr_cmd_clear                     (3'd2)
@@ -17,12 +14,5 @@
 `define cache_rw_cmd_iorw                       (4'd2)
 `define cache_rw_cmd_rb                         (4'd3)
 `define cache_rw_handleCtrCmd                   (4'd4)
-      
-`define SIZE_TO_DATA_RAM_ADDR_WIDTH             (log2(SIZE/(32/8*4)))
-`define SIZE_TO_TAG_RAM_ADDR_WIDTH              (DATA_RAM_ADDR_WIDTH-4)
-`define SIZE_TO_DRE_RAM_ADDR_WIDTH              (log2(SIZE/32)+1)
-`define SIZE_TO_TAG_ADDR_WIDTH                  (32-(DATA_RAM_ADDR_WIDTH+2))
-
-
 
 `endif
